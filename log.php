cb6af07f459df1ddb4be7eff6a6f69e858597af8
sagesparrows <89861649+sagesparrows@users.noreply.github.com>
2021-09-20 19:37:28 +0530

    Commite 19_37

---
+++ log.php
@@ -0,0 +1,20 @@
+﻿<?php
+// Report all PHP errors
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+$method = $_SERVER['REQUEST_METHOD'];
+//$req_dump = print_r($_REQUEST, true);
+if ('PUT' === $method) {
+	$decoded_input = file_get_contents("php://input");
+
+	//Here you have usual php array stored in $decoded_input. Do some stuff with it.
+	header('Content-type: application/json');
+	//echo json_encode($decoded_input);
+
+	$fp = file_put_contents('request.log', $decoded_input, FILE_APPEND);
+}
+//file_put_contents('request.log', $req_dump, FILE_APPEND);
+//print "ok";
+//return true;
+?>
